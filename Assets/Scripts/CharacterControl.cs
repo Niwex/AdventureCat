38b8b1dc56926327fd1f3962506d03af4816a069
Niwex <chelpanov98@gmail.com>
2021-02-27 14:41:48 +0700

    First commit

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    
+    public float speed = 3; 
+    public int maxHealth = 5;
+
+    public int getHealth { get { return currentHealth; } }
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+
+    float horizontal;
+    float vertical;
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = 3;
+    }
+
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+    }
+}
---
+++ Assets/Scripts/GoblinController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour
+{
+    private Vector3 startingposition;
+
+    private void Start()
+    {
+        startingposition = transform.position;
+    }
+
+    //private void Attack();
+
+}
