a1f3279901ca8e645a6521e18c55c68d28869251
Daniil Chelpanov <chelpanov98@gmail.com>
2021-03-28 20:14:54 +0700

    Add Attack animation, edit Attack func for main character.  change MonsterScript with getHit func and Die func

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,31 +5,47 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     //Stats
-    public float speed = 3; 
+    public float speed = 3;
     public int maxHealth = 5;
-
+    public float attackDmg = 20;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
     public int getHealth { get { return currentHealth; } }
     int currentHealth;
 
     //Attack stats
-    public Transform Attack1;
-    public float Attack1Radius;
+    public Transform attackPoint;
+    public float attackRadius;
+    public LayerMask enemyLayers;
+
     Rigidbody2D rigidbody2d;
-    
+    public Animator animator;
     float horizontal;
     float vertical;
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
 
+
+        attackRadius = 1.17f;
         currentHealth = 3;
     }
 
-    
+
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+
     }
 
     private void FixedUpdate()
@@ -43,11 +59,26 @@ public class CharacterControl : MonoBehaviour
     public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        
+
     }
 
     public void Attack()
     {
+        Debug.Log(this.animator);
+        this.animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayers);
 
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Hit " + enemy);
+            enemy.GetComponent<MonstersScript>().getHit(attackDmg);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 }
--- Assets/Scripts/MonstersScript.cs
+++ Assets/Scripts/MonstersScript.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public abstract class MonstersScript : MonoBehaviour
 {
+    public float maxHealth = 100;
+    public float currentHealth;
    public void Attack()
     {
+        //atack animation
+
+        //attack 
+    }
+
+    public void getHit(float dmg)
+    {
+        currentHealth -= dmg;
+
+        //hurt anim
+
+        //Die 
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        Debug.Log("die!" );
+
+        //die anim
+
 
     }
 }
