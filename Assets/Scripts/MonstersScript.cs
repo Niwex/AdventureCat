427e1089efe318e20c96cb82fb79ec2232ab54cc
Daniil Chelpanov <chelpanov98@gmail.com>
2021-03-29 21:43:26 +0700

    Add Skilet animation(walking/attacking) but controller does not work propetly. Add checkFollowRadius func and checkAttackRadius func in MonsterScript. SkelletonController: Add properties to Start func and edit Update func to try force skeleton move to player

--- Assets/Scripts/MonstersScript.cs
+++ Assets/Scripts/MonstersScript.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public abstract class MonstersScript : MonoBehaviour
 {
     public float maxHealth = 100;
-    public float currentHealth;
-   public void Attack()
+    protected float currentHealth;
+
+    float moveSpeed;
+    int attackDmg;
+    float attackRadius;
+
+    float followRadius;
+
+    public void Attack()
     {
         //atack animation
 
@@ -24,12 +31,73 @@ public abstract class MonstersScript : MonoBehaviour
             Die();
     }
 
-    void Die()
+    public void Die()
     {
         Debug.Log("die!" );
 
         //die anim
 
 
+    }
+
+    public void MoveToPlayer(float derectionX, float DerectionY)
+    {
+        transform.position += new Vector3(derectionX*getMoveSpeed() * Time.deltaTime,DerectionY * getMoveSpeed() * Time.deltaTime, 0f);
+    }
+
+    public void setMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+    public void setAttackDamage(int attdmg)
+    {
+        attackDmg = attdmg;
+    }
+    public float getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+    public int getAttackDamage()
+    {
+        return attackDmg;
+    }
+
+    //movement toward a player
+    public void setFollowRadius(float r)
+    {
+        followRadius = r;
+    }
+    //attack radius 
+    public void setAttackRadius(float r)
+    {
+        attackRadius = r;
+    }
+
+    //if player in radius move toward him 
+    public bool checkFollowRadius(float playerPosition, float enemyPosition)
+    {
+        if (Mathf.Abs(playerPosition - enemyPosition) < followRadius)
+        {
+            //player in range
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //if player in radius attack him
+    public bool checkAttackRadius(float playerPosition, float enemyPosition)
+    {
+        if (Mathf.Abs(playerPosition - enemyPosition) < attackRadius)
+        {
+            //in range for attack
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -5,21 +5,142 @@ using UnityEngine;
 public class SkeletonController : MonstersScript
 {
 
-    
-    Rigidbody2D rigidbody2d;
-    // Start is called before the first frame update
+
+    public float _moveSpeed;
+    public int _attackDamage;
+    public int _maxHealth;
+    public float _attackRadius;
+
+    //movement
+    public float _followRadius;
+    //end
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Animator enemyAnim;
+    SpriteRenderer enemySR;
+
     void Start()
     {
+        //get the player transform   
+        playerTransform = FindObjectOfType<CharacterControl>().GetComponent<Transform>();
+        //enemy animation and sprite renderer 
+        enemyAnim = gameObject.GetComponent<Animator>();
+        enemySR = GetComponent<SpriteRenderer>();
+        //set the variables
+        setMoveSpeed(_moveSpeed);
+        setAttackDamage(_attackDamage);
+        setAttackRadius(_attackRadius);
+        setFollowRadius(_followRadius);
         currentHealth = maxHealth;
-        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-            
-        Vector2 position = rigidbody2d.position;
-        
-        rigidbody2d.MovePosition(position);
+        if (checkFollowRadius(playerTransform.position.x, transform.position.x))
+        {
+            //if player in front of the enemies
+            if (playerTransform.position.y < transform.position.y)
+            {
+
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", true);
+                    enemyAnim.SetTrigger("AttackA");
+                }
+                else
+                {
+                    MoveToPlayer(0, -1);
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", false);
+                    //walk
+                    //enemyAnim.SetBool("Walking", true);
+                    enemyAnim.SetTrigger("Walking");
+                    enemySR.flipX = true;
+                }
+
+            }
+            //if player is behind enemies
+            else if (playerTransform.position.y > transform.position.y)
+            {
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //nemyAnim.SetBool("AttackA", true);
+                    enemyAnim.SetTrigger("AttackA");
+                }
+                else
+                {
+                    MoveToPlayer(0, 1);
+                    
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", false);
+                    //walk
+                    //enemyAnim.SetBool("Walking", true);
+                    enemyAnim.SetTrigger("Walking");
+                    enemySR.flipX = false;
+                }
+
+
+            }
+            else if (playerTransform.position.x > transform.position.x)
+            {
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //nemyAnim.SetBool("AttackA", true);
+                    enemyAnim.SetTrigger("AttackA");
+                }
+                else
+                {
+                    MoveToPlayer(-1, 0);
+
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", false);
+                    //walk
+                    //enemyAnim.SetBool("Walking", true);
+                    enemyAnim.SetTrigger("Walking");
+                    enemySR.flipX = false;
+                }
+
+
+            }
+            else if (playerTransform.position.x > transform.position.x)
+            {
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //nemyAnim.SetBool("AttackA", true);
+                    enemyAnim.SetTrigger("AttackA");
+                }
+                else
+                {
+                    MoveToPlayer(1, 0);
+
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", false);
+                    //walk
+                    //enemyAnim.SetBool("Walking", true);
+                    enemyAnim.SetTrigger("Walking");
+                    enemySR.flipX = false;
+                }
+
+
+            }
+        }
+        else
+        {
+            enemyAnim.SetBool("Walking", false);
+        }
+
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        //if (attackPoint == null)
+            //return;
+        Gizmos.DrawWireSphere(transform.position, _attackRadius);
+        Gizmos.DrawWireSphere(transform.position, _followRadius);
+
     }
 }
