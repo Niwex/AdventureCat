bfc3498b54bf7c49c196c3d0336a8793326a4894
Daniil Chelpanov <chelpanov98@gmail.com>
2021-04-14 19:33:19 +0700

    Made UI Interface
    Made healthbar and mana bar. Add monsters to scene 2 and 3

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] CharacterControl player;
+    private Image image;
+    float fillBarPercent;
+    void Start()
+    {
+        image = GetComponent<Image>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        fillBarPercent = player.getHealth / player.maxHealth;
+        image.fillAmount = fillBarPercent;
+    }
+}
