0f1e18b6aeacf238e7f06f04b7bab03721c4204a
Daniil Chelpanov <chelpanov98@gmail.com>
2021-04-17 00:51:58 +0700

    Add mana, manaRegen, manaCost,  changed CastSpells script a bit

--- Assets/Scripts/CastSpells.cs
+++ Assets/Scripts/CastSpells.cs
@@ -6,67 +6,88 @@ using UnityEngine;
 using UnityEngine.Windows.Speech;
 public class CastSpells : MonoBehaviour
 {
-  // Start is called before the first frame update
-  private KeywordRecognizer keywordRecognizer;
+    // Start is called before the first frame update
+    private KeywordRecognizer keywordRecognizer;
 
-  private Dictionary<string, Action> actions = new Dictionary<string, Action>();
-  public Rigidbody2D fireBall;
-  // private GameObject WaterAura;
-  public GameObject waterBall;
-  public GameObject activeWaterBall;
-  [SerializeField] int numberOfBalls;
-  [SerializeField] float spellRadius = 1.5f;
-  void Start()
-  {
-    actions.Add("fire", FireAttack);
-    keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
-    keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
-    keywordRecognizer.Start();
-  }
-  private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
-  {
-    Debug.Log(speech.text);
-    actions[speech.text].Invoke();
-  }
-  void WaterAttack()
-  {
-    Instantiate(waterBall, transform.position, Quaternion.identity, gameObject.transform);
-
-  }
-  void FireAttack()
-  {
-    // int fireBallNumber = 5;
-    for (int i = 0; i < numberOfBalls; i++)
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    public Rigidbody2D fireBall;
+    // private GameObject WaterAura;
+    public float maxMana;
+    public float currentMana;
+    public float manaRegen;
+    float manaRegenTick = 0f;
+    public GameObject waterBall;
+    public GameObject activeWaterBall;
+    [SerializeField] int numberOfBalls;
+    [SerializeField] float spellRadius = 1.5f;
+    Dictionary<string, int> spellCost = new Dictionary<string, int>();
+    void Start()
     {
-      // Rigidbody2D fireBall = fireBalls[i];
-      var angle = 360 / numberOfBalls;
-      var rot = Quaternion.Euler(0, 0, (angle * i)); //угол на который будет повёрнут фаербол
-      var distance = transform.position + (rot * new Vector3(spellRadius, 0f, 0f));
-
-      // Debug.Log(distance);
-      var fireballInsantiate = Instantiate(fireBall, distance, rot) as Rigidbody2D;
-      fireballInsantiate.AddForce(rot * new Vector2(1f, 0f) * 300f);
-      //объект, текущее положение игрока + угол умноженный на X , разворот относительно спрайта
+        spellCost.Add("fire", 10);
+        actions.Add("fire", FireAttack);
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
     }
-  }
-  // Update is called once per frame
-  void Update()
-  {
-    if (gameObject.transform.Find("waterball(Clone)"))
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
     {
-      // gameObject.transform.Find("waterball(Clone)").Rotate(0, 0, Time.deltaTime);
-      gameObject.transform.Find("waterball(Clone)").transform.Translate(Time.deltaTime * 5f, Time.deltaTime * 5f, Time.deltaTime);
-      gameObject.transform.Find("waterball(Clone)").transform.Rotate(0, 0, Time.deltaTime * 500f);
+        Debug.Log(speech.text);
+        if (spellCost[speech.text] <= currentMana)
+        {
+            actions[speech.text].Invoke();
+            currentMana -= spellCost[speech.text];
+        }
     }
-    if (Input.GetKeyDown(KeyCode.Mouse1))
+    void WaterAttack()
     {
-      WaterAttack();
-    }
-    if (Input.GetKeyDown(KeyCode.Mouse0))
-    {
-      FireAttack();
-      // int piMultiplier = 1;
+        Instantiate(waterBall, transform.position, Quaternion.identity, gameObject.transform);
 
     }
-  }
+    void FireAttack()
+    {
+        // int fireBallNumber = 5;
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            // Rigidbody2D fireBall = fireBalls[i];
+            var angle = 360 / numberOfBalls;
+            var rot = Quaternion.Euler(0, 0, (angle * i)); //угол на который будет повёрнут фаербол
+            var distance = transform.position + (rot * new Vector3(spellRadius, 0f, 0f));
+
+            // Debug.Log(distance);
+            var fireballInsantiate = Instantiate(fireBall, distance, rot) as Rigidbody2D;
+            fireballInsantiate.AddForce(rot * new Vector2(1f, 0f) * 300f);
+            //объект, текущее положение игрока + угол умноженный на X , разворот относительно спрайта
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.Find("waterball(Clone)"))
+        {
+            // gameObject.transform.Find("waterball(Clone)").Rotate(0, 0, Time.deltaTime);
+            gameObject.transform.Find("waterball(Clone)").transform.Translate(Time.deltaTime * 5f, Time.deltaTime * 5f, Time.deltaTime);
+            gameObject.transform.Find("waterball(Clone)").transform.Rotate(0, 0, Time.deltaTime * 500f);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            WaterAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireAttack();
+            // int piMultiplier = 1;
+
+        }
+        if(Time.time>manaRegenTick)
+            {
+            if (currentMana < maxMana)
+            {
+                if (currentMana + manaRegen < maxMana)
+                    currentMana += manaRegen;
+                else
+                    currentMana = maxMana;
+                manaRegenTick = Time.time + 1f;
+            } 
+        }
+    }
 }
