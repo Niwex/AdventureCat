6b89716d14d88388f4c07797791704f59b44fc80
Daniil Chelpanov <chelpanov98@gmail.com>
2021-04-19 01:27:15 +0700

    Build 0.2v
    Add spawners, Boss, Text with instructions, newMaps.

---
+++ Assets/Scripts/TextFieldTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFieldTimer : MonoBehaviour
+{
+    public float timeToWait = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, timeToWait);
+    }
+    
+}
