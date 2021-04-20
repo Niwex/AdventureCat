5debbd0a1516afce41727ed5865be7f3bc6fae11
Chelpanov Daniil <chelpanov98@gmail.com>
2021-04-20 22:13:50 +0700

    add scene choose im menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            SceneManager.LoadScene(2);
+        }   
+        if(Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            SceneManager.LoadScene(3);
+        }   
+        if(Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            SceneManager.LoadScene(4);
+        }   
+    }
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
